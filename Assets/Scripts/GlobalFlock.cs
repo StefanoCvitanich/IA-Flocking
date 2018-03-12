8d383474e7546b9ebb10412eb733d74a8c777765
StefanoCvitanich <stefanojc@gmail.com>
2018-03-12 16:31:37 -0300

    Creacion del Repo
    EL programa se cuenta andando. Se trata de Flocking (Inteligencia
    artificial); simulación de movimiento de Boids

---
+++ Assets/Scripts/GlobalFlock.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFlock : MonoBehaviour {
+
+	public GameObject fishPrefab;
+	public static int tankSize = 5;
+
+	public GameObject objectivePrefab;
+
+	static int numFish = 7;
+	public static GameObject[] allFish = new GameObject[numFish];
+
+	public static Vector3 goalPos = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < numFish; i++) {
+
+			Vector3 pos = new Vector3 (Random.Range (-tankSize, tankSize), Random.Range (-tankSize, tankSize), Random.Range (-tankSize, tankSize));
+		
+			allFish[i] = (GameObject) Instantiate(fishPrefab, pos, Quaternion.identity);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Random.Range (0, 10000) < 50) {
+		
+			goalPos = new Vector3 (Random.Range (-tankSize, tankSize), Random.Range (-tankSize, tankSize), Random.Range (-tankSize, tankSize));
+		
+			objectivePrefab.transform.position = goalPos;
+		}
+
+	}
+}
